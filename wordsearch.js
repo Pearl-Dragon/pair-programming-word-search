0e28eb968e291d416c74032d391613bc70743c96
Vesna Zaric <4vesnazaric@gmail.com>
2023-06-26 01:59:12 -0700

    Code passed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let verticalString = "";
+    for (let j = 0; j < letters.length; j++) {
+      verticalString += letters[j][i];
+    }
+    verticalJoin.push(verticalString);
+  }
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+
+};
+
+module.exports = wordSearch;
